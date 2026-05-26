ea2bd49957bdf5defa754fd6b7e98cee4f56f4a6
Thomas Hurney <hurneyt@amazon.com>
2026-05-26 13:08:51 -0700

    Phase 10 Batch 1: Convert 9 stateless class components to functions
    Converted: ForExpression, WhenExpression, OfExpression, OverExpression,
    TriggerExpressions, GroupByExpression, BucketLevelTriggerExpression,
    AnomalyDetectorTrigger, MetricExpression
    
    Signed-off-by: Thomas Hurney <hurneyt@amazon.com>

--- public/pages/CreateTrigger/components/BucketLevelTriggerExpression/BucketLevelTriggerExpression.js
+++ public/pages/CreateTrigger/components/BucketLevelTriggerExpression/BucketLevelTriggerExpression.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Field } from 'formik';
 import {
   EuiSmallButton,
@@ -23,113 +23,101 @@ const METRIC_FIELD_WIDTH = 300;
 const THRESHOLD_FIELD_WIDTH = 200;
 const VALUE_FIELD_WIDTH = 200;
 
-class BucketLevelTriggerExpression extends Component {
-  constructor(props) {
-    super(props);
-  }
+const BucketLevelTriggerExpression = ({
+  arrayHelpers,
+  queryMetrics,
+  index,
+  andOrConditionFieldName,
+  queryMetricFieldName,
+  enumFieldName,
+  valueFieldName,
+}) => {
+  const isFirstCondition = index === 0;
+  return (
+    <EuiFlexGroup
+      style={{
+        maxWidth: 1200,
+        paddingLeft: '10px',
+        paddingTop: '10px',
+        whiteSpace: 'nowrap',
+      }}
+      gutterSize={'m'}
+      alignItems={'flexStart'}
+    >
+      {!isFirstCondition ? (
+        <EuiFlexItem grow={false} style={{ width: `${AND_OR_FIELD_WIDTH}px` }}>
+          <Field name={andOrConditionFieldName}>
+            {({ field: { onBlur, ...rest }, form: { touched, errors } }) => (
+              <EuiCompressedFormRow
+                isInvalid={touched.andOrCondition && !!errors.andOrCondition}
+                error={errors.andOrCondition}
+              >
+                <EuiCompressedSelect options={AND_OR_CONDITION_OPTIONS} {...rest} />
+              </EuiCompressedFormRow>
+            )}
+          </Field>
+        </EuiFlexItem>
+      ) : null}
 
-  render() {
-    const {
-      arrayHelpers,
-      queryMetrics,
-      index,
-      andOrConditionFieldName,
-      queryMetricFieldName,
-      enumFieldName,
-      valueFieldName,
-    } = this.props;
-    const isFirstCondition = index === 0;
-    return (
-      <EuiFlexGroup
+      <EuiFlexItem
+        grow={false}
         style={{
-          maxWidth: 1200,
-          paddingLeft: '10px',
-          paddingTop: '10px',
-          whiteSpace: 'nowrap',
+          minWidth: isFirstCondition
+            ? `${METRIC_FIELD_WIDTH + AND_OR_FIELD_WIDTH + GUTTER_WIDTH}px`
+            : `${METRIC_FIELD_WIDTH}px`,
         }}
-        gutterSize={'m'}
-        alignItems={'flexStart'}
       >
-        {!isFirstCondition ? (
-          <EuiFlexItem grow={false} style={{ width: `${AND_OR_FIELD_WIDTH}px` }}>
-            <Field name={andOrConditionFieldName}>
-              {({ field: { onBlur, ...rest }, form: { touched, errors } }) => (
-                <EuiCompressedFormRow
-                  isInvalid={touched.andOrCondition && !!errors.andOrCondition}
-                  error={errors.andOrCondition}
-                >
-                  <EuiCompressedSelect options={AND_OR_CONDITION_OPTIONS} {...rest} />
-                </EuiCompressedFormRow>
-              )}
-            </Field>
-          </EuiFlexItem>
-        ) : null}
-
-        <EuiFlexItem
-          grow={false}
-          style={{
-            minWidth: isFirstCondition
-              ? `${METRIC_FIELD_WIDTH + AND_OR_FIELD_WIDTH + GUTTER_WIDTH}px`
-              : `${METRIC_FIELD_WIDTH}px`,
-          }}
-        >
-          <Field name={queryMetricFieldName} fullWidth={true}>
-            {({ field: { onBlur, ...rest }, form: { touched, errors } }) => (
-              <EuiCompressedFormRow
-                fullWidth={true}
-                label={isFirstCondition ? 'Metric' : null}
-                isInvalid={touched.queryMetric && !!errors.queryMetric}
-                error={errors.queryMetric}
-              >
-                <EuiCompressedSelect fullWidth={true} options={queryMetrics} {...rest} />
-              </EuiCompressedFormRow>
-            )}
-          </Field>
-        </EuiFlexItem>
-
-        <EuiFlexItem grow={false} style={{ width: `${THRESHOLD_FIELD_WIDTH}px` }}>
-          <Field name={enumFieldName} fullWidth={true}>
-            {({ field: { onBlur, ...rest }, form: { touched, errors } }) => (
-              <EuiCompressedFormRow
-                fullWidth={true}
-                label={isFirstCondition ? 'Threshold' : null}
-                isInvalid={touched.thresholdEnum && !!errors.thresholdEnum}
-                error={errors.thresholdEnum}
-              >
-                <EuiCompressedSelect options={THRESHOLD_ENUM_OPTIONS} {...rest} />
-              </EuiCompressedFormRow>
-            )}
-          </Field>
-        </EuiFlexItem>
-
-        <EuiFlexItem grow={false} style={{ width: `${VALUE_FIELD_WIDTH}px` }}>
-          <Field name={valueFieldName}>
-            {({ field, form: { touched, errors } }) => (
-              <EuiCompressedFormRow
-                label={isFirstCondition ? 'Value' : null}
-                isInvalid={touched.thresholdValue && !!errors.thresholdValue}
-                error={errors.thresholdValue}
-              >
-                <EuiCompressedFieldNumber {...field} />
-              </EuiCompressedFormRow>
-            )}
-          </Field>
-        </EuiFlexItem>
-        {!isFirstCondition ? (
-          <EuiFlexItem grow={false}>
-            <EuiSmallButton
-              color={'danger'}
-              onClick={() => {
-                arrayHelpers.remove(index);
-              }}
+        <Field name={queryMetricFieldName} fullWidth={true}>
+          {({ field: { onBlur, ...rest }, form: { touched, errors } }) => (
+            <EuiCompressedFormRow
+              fullWidth={true}
+              label={isFirstCondition ? 'Metric' : null}
+              isInvalid={touched.queryMetric && !!errors.queryMetric}
+              error={errors.queryMetric}
             >
-              Remove condition
-            </EuiSmallButton>
-          </EuiFlexItem>
-        ) : null}
-      </EuiFlexGroup>
-    );
-  }
-}
+              <EuiCompressedSelect fullWidth={true} options={queryMetrics} {...rest} />
+            </EuiCompressedFormRow>
+          )}
+        </Field>
+      </EuiFlexItem>
+
+      <EuiFlexItem grow={false} style={{ width: `${THRESHOLD_FIELD_WIDTH}px` }}>
+        <Field name={enumFieldName} fullWidth={true}>
+          {({ field: { onBlur, ...rest }, form: { touched, errors } }) => (
+            <EuiCompressedFormRow
+              fullWidth={true}
+              label={isFirstCondition ? 'Threshold' : null}
+              isInvalid={touched.thresholdEnum && !!errors.thresholdEnum}
+              error={errors.thresholdEnum}
+            >
+              <EuiCompressedSelect options={THRESHOLD_ENUM_OPTIONS} {...rest} />
+            </EuiCompressedFormRow>
+          )}
+        </Field>
+      </EuiFlexItem>
+
+      <EuiFlexItem grow={false} style={{ width: `${VALUE_FIELD_WIDTH}px` }}>
+        <Field name={valueFieldName}>
+          {({ field, form: { touched, errors } }) => (
+            <EuiCompressedFormRow
+              label={isFirstCondition ? 'Value' : null}
+              isInvalid={touched.thresholdValue && !!errors.thresholdValue}
+              error={errors.thresholdValue}
+            >
+              <EuiCompressedFieldNumber {...field} />
+            </EuiCompressedFormRow>
+          )}
+        </Field>
+      </EuiFlexItem>
+      {!isFirstCondition ? (
+        <EuiFlexItem grow={false}>
+          <EuiSmallButton color={'danger'} onClick={() => arrayHelpers.remove(index)}>
+            Remove condition
+          </EuiSmallButton>
+        </EuiFlexItem>
+      ) : null}
+    </EuiFlexGroup>
+  );
+};
 
 export default BucketLevelTriggerExpression;
--- public/pages/CreateTrigger/containers/DefineTrigger/AnomalyDetectorTrigger.js
+++ public/pages/CreateTrigger/containers/DefineTrigger/AnomalyDetectorTrigger.js
@@ -13,137 +13,116 @@ import { EmptyFeaturesMessage } from '../../../CreateMonitor/components/AnomalyD
 import { EmptyDetectorMessage } from '../../../CreateMonitor/components/AnomalyDetectors/EmptyDetectorMessage/EmptyDetectorMessage';
 import { PREVIEW_ERROR_TYPE } from '../../../../utils/constants';
 
-/**
-  *
-  * @param {string} err if there is any exception or error running preview API,
-     err is not empty.
-  * @param {*} features detector features
-  * @returns error type
- */
 function getPreviewErrorType(err, features) {
   if (features === undefined || features.length == 0) {
     return PREVIEW_ERROR_TYPE.NO_FEATURE;
   }
-
   const enabledFeatures = features.filter((feature) => feature.featureEnabled);
   if (enabledFeatures.length == 0) {
     return PREVIEW_ERROR_TYPE.NO_ENABLED_FEATURES;
   }
-
-  // if error is a non-empty string, return it.
   if (err) return PREVIEW_ERROR_TYPE.PREVIEW_EXCEPTION;
-
-  // sparse data
   return PREVIEW_ERROR_TYPE.SPARSE_DATA;
 }
 
-class AnomalyDetectorTrigger extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { adValues, detectorId, fieldPath, flyoutMode } = this.props;
-    return (
-      <div style={flyoutMode ? {} : { padding: '0px 10px' }}>
-        <AnomalyDetectorData
-          detectorId={detectorId}
-          render={(anomalyData) => {
-            const features = _.get(anomalyData, 'detector.featureAttributes', []);
-            const isHCDetector = !_.isEmpty(_.get(anomalyData, 'detector.categoryField', []));
-            const previewErrorType = getPreviewErrorType(anomalyData.error, features);
+const AnomalyDetectorTrigger = ({ adValues, detectorId, fieldPath, flyoutMode }) => (
+  <div style={flyoutMode ? {} : { padding: '0px 10px' }}>
+    <AnomalyDetectorData
+      detectorId={detectorId}
+      render={(anomalyData) => {
+        const features = _.get(anomalyData, 'detector.featureAttributes', []);
+        const isHCDetector = !_.isEmpty(_.get(anomalyData, 'detector.categoryField', []));
+        const previewErrorType = getPreviewErrorType(anomalyData.error, features);
 
-            // using lodash.get without worrying about whether an intermediate property is null or undefined.
-            if (_.get(anomalyData, 'anomalyResult.anomalies', []).length > 0) {
-              return (
-                <React.Fragment>
-                  <TriggerExpressions
-                    thresholdValue={adValues.anomalyGradeThresholdValue}
-                    thresholdEnum={adValues.anomalyGradeThresholdEnum}
-                    keyFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdEnum`}
-                    valueFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdValue`}
-                    label="Anomaly grade threshold"
+        if (_.get(anomalyData, 'anomalyResult.anomalies', []).length > 0) {
+          return (
+            <React.Fragment>
+              <TriggerExpressions
+                thresholdValue={adValues.anomalyGradeThresholdValue}
+                thresholdEnum={adValues.anomalyGradeThresholdEnum}
+                keyFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdEnum`}
+                valueFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdValue`}
+                label="Anomaly grade threshold"
+              />
+              {!flyoutMode && (
+                <>
+                  <EuiSpacer size="xs" />
+                  <AnomaliesChart
+                    showTitle={false}
+                    showSettings={false}
+                    startDateTime={anomalyData.previewStartTime}
+                    endDateTime={anomalyData.previewEndTime}
+                    anomalies={anomalyData.anomalyResult.anomalies}
+                    isLoading={anomalyData.isLoading}
+                    displayGrade
+                    annotationData={[{ dataValue: adValues.anomalyGradeThresholdValue }]}
                   />
-                  {!flyoutMode && (
-                    <>
-                      <EuiSpacer size="xs" />
-                      <AnomaliesChart
-                        showTitle={false}
-                        showSettings={false}
-                        startDateTime={anomalyData.previewStartTime}
-                        endDateTime={anomalyData.previewEndTime}
-                        anomalies={anomalyData.anomalyResult.anomalies}
-                        isLoading={anomalyData.isLoading}
-                        displayGrade
-                        annotationData={[{ dataValue: adValues.anomalyGradeThresholdValue }]}
-                      />
-                    </>
-                  )}
-                  <EuiSpacer size="m" />
-                  <TriggerExpressions
-                    thresholdValue={adValues.anomalyConfidenceThresholdValue}
-                    thresholdEnum={adValues.anomalyConfidenceThresholdEnum}
-                    keyFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdEnum`}
-                    valueFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdValue`}
-                    label="Anomaly confidence threshold"
+                </>
+              )}
+              <EuiSpacer size="m" />
+              <TriggerExpressions
+                thresholdValue={adValues.anomalyConfidenceThresholdValue}
+                thresholdEnum={adValues.anomalyConfidenceThresholdEnum}
+                keyFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdEnum`}
+                valueFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdValue`}
+                label="Anomaly confidence threshold"
+              />
+              {!flyoutMode && (
+                <>
+                  <EuiSpacer size="xs" />
+                  <AnomaliesChart
+                    showTitle={false}
+                    showSettings={false}
+                    startDateTime={anomalyData.previewStartTime}
+                    endDateTime={anomalyData.previewEndTime}
+                    anomalies={anomalyData.anomalyResult.anomalies}
+                    isLoading={anomalyData.isLoading}
+                    displayConfidence
+                    annotationData={[{ dataValue: adValues.anomalyConfidenceThresholdValue }]}
                   />
-                  {!flyoutMode && (
-                    <>
-                      <EuiSpacer size="xs" />
-                      <AnomaliesChart
-                        showTitle={false}
-                        showSettings={false}
-                        startDateTime={anomalyData.previewStartTime}
-                        endDateTime={anomalyData.previewEndTime}
-                        anomalies={anomalyData.anomalyResult.anomalies}
-                        isLoading={anomalyData.isLoading}
-                        displayConfidence
-                        annotationData={[{ dataValue: adValues.anomalyConfidenceThresholdValue }]}
-                      />
-                    </>
-                  )}
-                </React.Fragment>
-              );
-            } else if (_.isEmpty(detectorId)) {
-              return <EmptyDetectorMessage />;
-            } else if (
-              previewErrorType === PREVIEW_ERROR_TYPE.EXCEPTION ||
-              previewErrorType === PREVIEW_ERROR_TYPE.SPARSE_DATA
-            ) {
-              return (
-                <React.Fragment>
-                  <TriggerExpressions
-                    thresholdValue={adValues.anomalyGradeThresholdValue}
-                    thresholdEnum={adValues.anomalyGradeThresholdEnum}
-                    keyFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdEnum`}
-                    valueFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdValue`}
-                    label="Anomaly grade threshold"
-                  />
-                  <EuiSpacer size="m" />
-                  <TriggerExpressions
-                    thresholdValue={adValues.anomalyConfidenceThresholdValue}
-                    thresholdEnum={adValues.anomalyConfidenceThresholdEnum}
-                    keyFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdEnum`}
-                    valueFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdValue`}
-                    label="Anomaly confidence threshold"
-                  />
-                </React.Fragment>
-              );
-            } else {
-              return (
-                <EmptyFeaturesMessage
-                  detectorId={detectorId}
-                  isLoading={anomalyData.isLoading}
-                  error={anomalyData.error}
-                  isHCDetector={isHCDetector}
-                  previewErrorType={previewErrorType}
-                />
-              );
-            }
-          }}
-        />
-      </div>
-    );
-  }
-}
+                </>
+              )}
+            </React.Fragment>
+          );
+        } else if (_.isEmpty(detectorId)) {
+          return <EmptyDetectorMessage />;
+        } else if (
+          previewErrorType === PREVIEW_ERROR_TYPE.EXCEPTION ||
+          previewErrorType === PREVIEW_ERROR_TYPE.SPARSE_DATA
+        ) {
+          return (
+            <React.Fragment>
+              <TriggerExpressions
+                thresholdValue={adValues.anomalyGradeThresholdValue}
+                thresholdEnum={adValues.anomalyGradeThresholdEnum}
+                keyFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdEnum`}
+                valueFieldName={`${fieldPath}anomalyDetector.anomalyGradeThresholdValue`}
+                label="Anomaly grade threshold"
+              />
+              <EuiSpacer size="m" />
+              <TriggerExpressions
+                thresholdValue={adValues.anomalyConfidenceThresholdValue}
+                thresholdEnum={adValues.anomalyConfidenceThresholdEnum}
+                keyFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdEnum`}
+                valueFieldName={`${fieldPath}anomalyDetector.anomalyConfidenceThresholdValue`}
+                label="Anomaly confidence threshold"
+              />
+            </React.Fragment>
+          );
+        } else {
+          return (
+            <EmptyFeaturesMessage
+              detectorId={detectorId}
+              isLoading={anomalyData.isLoading}
+              error={anomalyData.error}
+              isHCDetector={isHCDetector}
+              previewErrorType={previewErrorType}
+            />
+          );
+        }
+      }}
+    />
+  </div>
+);
 
 export { AnomalyDetectorTrigger };
